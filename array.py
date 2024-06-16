0d29f7445e5b3433bc483f033f0081fb3b31ed84
Anil Patel2/CMG/PBP <anil.patel2@Cipla.com>
2024-06-16 17:39:18 +0530

    My 1st commit

---
+++ array.py
@@ -0,0 +1,21 @@
+import array
+
+
+my_array = array.array('i')
+print(my_array)
+
+my_array1 = array.array('i',[1,2,3,4])
+print(my_array1)
+
+my_array1.insert(0,6)
+print(my_array1)
+
+
+
+# import numpy as np
+
+# np_array = np.array([], dtype=int)
+# print(np_array)
+
+# np_array1 = np.array([1,2,3,4, 'a'], dtype=int)
+# print(np_array1)
